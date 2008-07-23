1f9f2324f97103234e1aa6fbd504fb5cc84e65e9
marko82 <marko82@90fd9065-a65c-487c-a5f0-720db8400081>
2008-07-23 22:59:18 +0000

    Taking back changes from r97
    git-svn-id: https://audacious-dvb.svn.sourceforge.net/svnroot/audacious-dvb/trunk@142 90fd9065-a65c-487c-a5f0-720db8400081

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
+libtoolize --force
 aclocal
 autoheader
 automake --add-missing --copy
 autoconf
-libtoolize --force
 
 echo "Now you are ready to run ./configure (check '--help' for options)"
